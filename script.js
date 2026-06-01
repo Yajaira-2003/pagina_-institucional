0ff61a7653c1bdad1f9d929dddf1fbdbc3a986b9
Yajaira-2003 <yajaira.yaguapaz.vera@utelvt.edu.ec>
2026-06-01 09:59:24 -0500

    script.js

--- script.js
+++ script.js
@@ -365,3 +365,11 @@ const navMenu = document.getElementById('nav-menu');
 menuToggle.addEventListener('click', () => {
     navMenu.classList.toggle('active');
 });
+
+const menuToggle = document.getElementById('menu-toggle');
+const navMenu = document.getElementById('nav-menu');
+
+menuToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
+
